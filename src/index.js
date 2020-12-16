864dc7afa628d1b887b95b2f019f26f984e3601a
Leevi Alho <54808340+LeeviAlho@users.noreply.github.com>
2020-12-16 14:43:44 +0200

    testi

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(index.html);
+  res.sendFile("index.html");
 });
 app.get("/qwe", (req, res) => {
   res.send("qweqweqwe");

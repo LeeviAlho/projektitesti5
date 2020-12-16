8fa2b8bcd7086b47f10548cea82167957fb5af62
Leevi Alho <54808340+LeeviAlho@users.noreply.github.com>
2020-12-16 14:41:05 +0200

    test

--- src/index.js
+++ src/index.js
@@ -3,27 +3,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<html>");
-  res.send('<script src="src/index.js"></script>');
-  res.send("<head>");
-  res.send('<meta charset="UTF-8" />');
-  res.send("<title>tic tac toe table</title>");
-  res.send("</head>");
-
-  res.send("<body>");
-  res.send('<div class="w3-container" id="board"></div>');
-  res.send('<div id="table"></div>');
-  res.send("<!Tic Tac Toe table>");
-  res.send('<p id="counter"></p>');
-
-  res.send('<div id="wincondition"></div>');
-  res.send("</body>");
-
-  res.send('<script src="src/table.js"></script>');
-  res.send('<script src="src/buttons.js"></script>');
-  res.send("</html>);");
+  res.sendFile(index.html);
 });
-
 app.get("/qwe", (req, res) => {
   res.send("qweqweqwe");
 });

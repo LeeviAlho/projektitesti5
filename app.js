ed0d9b92b37694fb5ff8e5423636ddc5b98c22af
Leevi Alho <54808340+LeeviAlho@users.noreply.github.com>
2020-12-19 13:34:32 +0200

    testi

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+var createError = require("http-errors");
 var express = require("express");
 var cookieParser = require("cookie-parser");
+var path = require("path");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
 
 var app = express();
 
@@ -13,22 +18,43 @@ mongoose.Promise = Promise;
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-// View engine setup
-app.set("view engine", "ejs");
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-// Without middleware
-app.get("/", function (req, res) {
-  // Rendering home.ejs page
-  res.render("home");
-  return 0;
-});
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+//use index
+app.use("/", indexRouter);
 
 app.get("/qwe", (req, res) => {
   res.send("qweqweqwe");
   return 0;
 });
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
 app.listen(PORT, function (err) {
   if (err) console.log(err);
   console.log("Server listening on PORT", PORT);
 });
+
+module.exports = app;
